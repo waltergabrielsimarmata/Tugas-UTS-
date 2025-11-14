04366b9e8901e424ad1a8e5b9a3e18308981939c
waltergabrielsimarmata <if24.waltersimarmata@mhs.ubpkarawang.ac.id>
2025-11-14 21:36:24 +0700

    LuasLingkaran.php

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,40 +1,17 @@
 <?php
 
-class Lingkaran
-{
-    // Pertemuan 3: Property dan Constant
-    public $jariJari;
-    const PI = 3.14;
+namespace App\Math;
 
-    // Pertemuan 5: Constructor
-    public function __construct($r)
-    {
-        $this->jariJari = $r;
-        echo "Object Lingkaran dibuat dengan jari-jari: $r <br>";
+class LuasLingkaran {
+
+    public const PHI = 3.14;
+    private int $jari;
+
+    public function __construct($j = 10) {
+        $this->jari = $j;
     }
 
-    // Pertemuan 4: Method
-    public function hitungLuas()
-    {
-        return self::PI * $this->jariJari * $this->jariJari;
-    }
-
-    // Pertemuan 4: Static Method
-    public static function info()
-    {
-        return "Class untuk menghitung luas lingkaran";
-    }
-
-    // Pertemuan 6: Access modifier (private)
-    private function contohPrivate()
-    {
-        return "Ini method private, tidak bisa dipanggil dari luar class.";
-    }
-
-    // Pertemuan 5: Destructor
-    public function __destruct()
-    {
-        echo "<br>Object Lingkaran dihancurkan.";
+    public function getLuas() {
+        return self::PHI * ($this->jari * $this->jari);
     }
 }
-?>
